f44308cf7d71328986fa76feb944ed54a810b99a
David Sotto Mayor <ddsmmf@gmail.com>
2021-09-16 11:47:05 -0400

    Fixing some bugs in the application

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb+srv://david:0aouOZQelm6nb9XE@cluster0.pr7b4.mongodb.net/blog?authSource=admin&replicaSet=atlas-2dh8x7-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true',
   cookieKey: '123123123',
 };
