948410831f4334c2c7f11f768e80b373fa78482a
David Sotto Mayor <ddsmmf@gmail.com>
2021-09-20 09:38:43 -0400

    Deleting all the redis code of Blogs routes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,36 +14,9 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    // Redis Goal: Catch queried data when we search at the first time in the DB and store it.
-    //             When we need it, redis will return this data without consulting the database.
-
-    const redis = require('redis'); // npm install --save redis
-    const redisUrl = 'redis://127.0.0.1:6379' // localhost
-    const redisClient = redis.createClient(redisUrl);
-    // Now, we wanna add promise to a 'redisClient.get', actually this function only work with callback.
-    const util = require('util');
-    redisClient.get = util.promisify(redisClient.get);
-
-    // Redis Flow 1: Do we have any cached data in redis related to this query?
-    const cachedBlogs = await redisClient.get(req.user.id);
-    const parsedBlogs = JSON.parse(cachedBlogs); // JSON to JS Object!
-    
-    console.log(parsedBlogs);
-
-    // Redis Flow 2: If yes, respond to the resquest right away and return it.
-    if(parsedBlogs){
-      console.log('SERVING REDIS');
-      return res.status(200).json({parsedBlogs: parsedBlogs});
-    }
-
-    // Redis Flow 3: If no, we need to responde to the resquest and update the cache 
-    //               to store data.
     const blogs = await Blog.find({ _user: req.user.id });
 
-    console.log('SERVING DB');
     res.send(blogs);
-
-    redisClient.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
