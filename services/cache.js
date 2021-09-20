d0987e84807380d042b8b60f169b9973c712e5b3
David Sotto Mayor <ddsmmf@gmail.com>
2021-09-20 09:57:56 -0400

    Generating a (unique) redisKey

--- services/cache.js
+++ services/cache.js
@@ -5,5 +5,14 @@ const exec = mongoose.Query.prototype.exec;
 mongoose.Query.prototype.exec = function() {
     console.log('Execution into the mongoose.Query.prototype.exec');
 
+    // To generate UNIQUE KEYS, we can combine the query with the collection,
+    // both of mongoose!
+    console.log(this.getQuery());
+    console.log(this.mongooseCollection.name);
+
+    const redisKey = Object.assign({}, this.getQuery(), { collection: this.mongooseCollection.name });
+    console.log(redisKey);
+
+
     return exec.apply(this, arguments);
 }
