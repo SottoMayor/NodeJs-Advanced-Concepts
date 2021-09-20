26958a748cf798b454460db6a76c3681375e9900
David Sotto Mayor <ddsmmf@gmail.com>
2021-09-20 10:17:18 -0400

    Adding redis in 'services/cache' and writing the redis flow into it.

--- services/cache.js
+++ services/cache.js
@@ -1,18 +1,31 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const redisClient = redis.createClient(redisUrl);
+redisClient.get = util.promisify(redisClient.get);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function() {
-    console.log('Execution into the mongoose.Query.prototype.exec');
+mongoose.Query.prototype.exec = async function () {
+    // Generating redis UNIQUE KEYS.
+    const redisKey = JSON.stringify(
+        Object.assign({}, this.getQuery(), {
+            collection: this.mongooseCollection.name,
+        })
+    );
 
-    // To generate UNIQUE KEYS, we can combine the query with the collection,
-    // both of mongoose!
-    console.log(this.getQuery());
-    console.log(this.mongooseCollection.name);
+    // Redis Flow 1: Verify if the 'redisKey' already exists in redis
+    const cachedValue = await redisClient.get(redisKey);
 
-    const redisKey = Object.assign({}, this.getQuery(), { collection: this.mongooseCollection.name });
-    console.log(redisKey);
+    // Redis Flow 2: The 'redisKey' already exists! Return the result stored in redis.
+    if(cachedValue){
+        console.log(cachedValue);
+    }
 
+    // Redis Flow 3: The 'redisKey' not exists yet! Query the result and store it into redis.
+    const result = await exec.apply(this, arguments);
+    console.log(arguments);
 
-    return exec.apply(this, arguments);
-}
+};
