bd70796c9a48e1b5ca166b51de62e4a92553d022
David Sotto Mayor <ddsmmf@gmail.com>
2021-10-04 07:51:54 -0400

    Testing the header - The logo name appears correctly

--- tests/header.test.js
+++ tests/header.test.js
@@ -5,7 +5,13 @@ describe('Testing the header', () => {
     test('Launching Chromium Instances', async () => {
 
         const browser = await puppeteer.launch({ headless: false });
-        const newPage = browser.newPage();
+        const newPage = await browser.newPage();
+
+        await newPage.goto('localhost:3000');
+
+        // Verifying if 'Blogster' name appears in the header
+        const headerText = await newPage.$eval('a.brand-logo', element => element.innerHTML);
+        expect(headerText).toEqual('Blogster');
     })
 
 })
