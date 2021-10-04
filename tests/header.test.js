16e8b9ba18a527023b3db6254790fe25b8bbf4ed
David Sotto Mayor <ddsmmf@gmail.com>
2021-10-04 08:16:24 -0400

    A little refactoring in header test file

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,13 +1,20 @@
 const puppeteer = require('puppeteer'); // With this, we can work with chromium
 
-describe('Testing the header', () => {
+// Now, this variables are in global scope
+let newPage, browser;
+
+// This will be executes before each running test
+beforeEach(async () => {
+    browser = await puppeteer.launch({ headless: false });
+    newPage = await browser.newPage();
+
+    await newPage.goto('localhost:3000');
+})
+
+
+describe('Testing the header', async () => {
 
     test('Launching Chromium Instances', async () => {
-
-        const browser = await puppeteer.launch({ headless: false });
-        const newPage = await browser.newPage();
-
-        await newPage.goto('localhost:3000');
 
         // Verifying if 'Blogster' name appears in the header
         const headerText = await newPage.$eval('a.brand-logo', element => element.innerHTML);
