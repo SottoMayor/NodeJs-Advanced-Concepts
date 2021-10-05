2485c1ba26b67e4f6ef9068982c11a2503691abd
David Sotto Mayor <ddsmmf@gmail.com>
2021-10-05 08:39:19 -0400

    Testing the Header - Checking if when click login trigger oauth

--- tests/header.test.js
+++ tests/header.test.js
@@ -17,11 +17,21 @@ afterEach(async () => browser.close());
 
 describe('Testing the header', async () => {
 
-    test('Launching Chromium Instances', async () => {
+    test('Verifying if "Blogster" name appears in the header', async () => {
 
-        // Verifying if 'Blogster' name appears in the header
         const headerText = await newPage.$eval('a.brand-logo', element => element.innerHTML);
         expect(headerText).toEqual('Blogster');
+
+    });
+
+    test('Checking if when click login trigger oauth', async () => {
+        // Click in the oauth button
+        await newPage.click('.right a');
+        //Extracting the page url
+        const url = await newPage.url();
+
+        // Making sure the url matches the google oauth url
+        expect(url).toMatch(/accounts\.google\.com/);
     })
 
 })
